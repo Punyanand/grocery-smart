5bdfaec9549a533ed5a4dbce0410853186b39779
sripriya497 <sripriyadharshini497@gmail.com>
2025-04-22 16:35:10 -0500

    Update to fix dounle slash issue with API URL

--- frontend/grocery-app-list/src/config.js
+++ frontend/grocery-app-list/src/config.js
@@ -1,3 +1,4 @@
 const API_BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:10000";
 
-export default API_BASE_URL;
+// Ensure the URL doesn't end with a slash
+export default API_BASE_URL.endsWith('/') ? API_BASE_URL.slice(0, -1) : API_BASE_URL;
